632c90f62673db472d64884d870cae6f7db173f8
saiful298 <prog.saiful@gmail.com>
2022-02-14 22:46:36 +0600

    first commit

---
+++ script.js
@@ -0,0 +1,17 @@
+function callFunction(data) {
+    const incomePut = document.getElementById('income-put').value;
+    const foodPut = document.getElementById('food-put').value;
+    const rentPut = document.getElementById('rent-put').value;
+    const clothPut = document.getElementById('clothes-put').value;
+    // if (incomePut != "")
+    const totalPrice = parseFloat(foodPut) + parseFloat(rentPut) + parseFloat(clothPut);
+    const balancePrice = incomePut - totalPrice;
+    document.getElementById('total-exp').innerText = totalPrice;
+    document.getElementById('balance').innerText = balancePrice;
+    document.getElementById('balanceInput').setAttribute("value", balancePrice);
+    // Validation
+    if (incomePut < 0 || foodPut < 0 || rentPut < 0 || clothPut < 0) {
+        alert('stop here')
+    }
+
+};
